7a8f02165d448b67d42fe57dbe905f9f6974d94a
prisha1111 <122594618+prisha1111@users.noreply.github.com>
2023-03-23 09:20:32 +0530

    Add files via upload

---
+++ 2darrays.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n[3][3],r,c;
+	for(r=0;r<3;r++)
+	{
+		for(c=0;c<3;c++)
+		{
+			printf("\n enter a number");
+			scanf("%d",&n[r][c]);
+		}
+	}
+		printf("\n the given matrix");
+		for(r=0;r<3;r++)
+		{
+			printf("\n");
+			for(c=0;c<3;c++)
+			{
+				printf(" \t %d",n[r][c]);
+	     	}
+		}
+	
+	
+}
